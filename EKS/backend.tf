fc496699b65671e2b175ff5577220dcc6f31153e
VishAstra <vshnu911@gmail.com>
2024-02-25 12:51:01 +0530

    fixed the errors

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "cicd-terraform-eks"
+    bucket = "jenkins-eks-terraform"
     key    = "eks/terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 }
--- Jenkins_Server/backend.tf
+++ Jenkins_Server/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "cicd-terraform-eks"
+    bucket = "jenkins-eks-terraform"
     key    = "jenkins/terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 }
